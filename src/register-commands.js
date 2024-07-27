fe144af3752c6fe8a3af92acc4c6414a09852e46
Not Gius <giuseppe>
2024-07-27 12:26:05 -0300

    feat: slash commands

--- src/register-commands.js
+++ src/register-commands.js
@@ -1,42 +1,47 @@
-require('dotenv').config();
-const { REST, Routes, ApplicationCommandOptionType } = require('discord.js');
+require("dotenv").config();
+const { REST, Routes, ApplicationCommandOptionType } = require("discord.js");
 
 // All the functions to be registered on the server
 // is gonna to appear on / menu, need to chenge on index function
 const commands = [
   {
-    name: 'add',
-    description: 'soma de numeros',
+    name: "add",
+    description: "soma de numeros",
     options: [
       {
-        name: 'primeiro-number',
-        description: 'the primeiro number',
+        name: "first-number",
+        description: "the first number",
         type: ApplicationCommandOptionType.Number,
-        require: true
-      },
+        require: false,
+        autocomplete: true
+      },  
       {
-        name: 'segundo-number',
-        description: 'the segundo number',
+        name: "second-number",
+        description: "the second number",
         type: ApplicationCommandOptionType.Number,
-        require: true
-      }
+        require: false,
+        autocomplete: true
+      },
     ],
-  }
+  },
 ];
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 // Function to register the commands as slash commands
 (async () => {
   try {
-    console.log('Registering slash commands');
+    console.log("Registering slash commands");
 
     await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
       { body: commands }
     );
 
-    console.log('Slash commands were registered successfully!');
+    console.log("Slash commands were registered successfully!");
   } catch (error) {
     console.error(`There was an error: ${error}`);
   }
